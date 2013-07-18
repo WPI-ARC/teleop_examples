2ba313d214f5451f0952a28e8a7fb2d1de63b69d
Calder Phillips-Grafflin <calder.pg@gmail.com>
2013-07-18 18:50:46 -0400

    Resized markers, combined cartesian and execution controls into one visible marker

--- hubo_marker_teleop/src/hubo_marker_teleop/backup_hubo_marker_teleop.py
+++ hubo_marker_teleop/src/hubo_marker_teleop/backup_hubo_marker_teleop.py
@@ -69,10 +69,16 @@ class HuboMarkerTeleop:
         #for marker_name in self.server.marker_contexts.keys():
         #    self.server.erase(marker_name)
         self.server.clear()
+        left_arm_name = "LEFT_ARM"
         left_gripper_name = "LEFT_HAND"
+        right_arm_name = "RIGHT_ARM"
         right_gripper_name = "RIGHT_HAND"
+        left_arm_marker = self.make_arm_imarker(self.left_arm_pose, left_arm_name)
+        right_arm_marker = self.make_arm_imarker(self.right_arm_pose, right_arm_name)
         left_gripper_marker = self.make_gripper_imarker(self.left_arm_pose, left_gripper_name)
         right_gripper_marker = self.make_gripper_imarker(self.right_arm_pose, right_gripper_name)
+        self.server.insert(left_arm_marker, self.l_arm_feedback_cb)
+        self.server.insert(right_arm_marker, self.r_arm_feedback_cb)
         self.server.insert(left_gripper_marker, self.l_gripper_feedback_cb)
         self.server.insert(right_gripper_marker, self.r_gripper_feedback_cb)
         self.left_gripper_handler.apply(self.server, left_gripper_name)
@@ -86,7 +92,7 @@ class HuboMarkerTeleop:
         elif (event_type == feedback.MOUSE_UP):
             pass
         elif (event_type == feedback.POSE_UPDATE):
-            self.left_arm_pose.pose = self.clip_to_arm_bounds(feedback.pose, 'LEFT')
+            pass
         elif (event_type == feedback.MENU_SELECT):
             if (self.gripper_options[feedback.menu_entry_id - 1] == "CLOSE"):
                 #self.l_gripper_client.send_goal(self.close_goal)
@@ -110,7 +116,7 @@ class HuboMarkerTeleop:
         elif (event_type == feedback.MOUSE_UP):
             pass
         elif (event_type == feedback.POSE_UPDATE):
-            self.right_arm_pose.pose = self.clip_to_arm_bounds(feedback.pose, 'RIGHT')
+            pass
         elif (event_type == feedback.MENU_SELECT):
             if (self.gripper_options[feedback.menu_entry_id - 1] == "CLOSE"):
                 #self.r_gripper_client.send_goal(self.close_goal)
@@ -125,6 +131,30 @@ class HuboMarkerTeleop:
                 rospy.logerr("Unrecognized menu option")
         else:
             rospy.logerr("Right gripper - unrecognized feedback type - " + str(feedback.event_type))
+        self.update()
+
+    def l_arm_feedback_cb(self, feedback):
+        event_type = feedback.event_type
+        if (event_type == feedback.POSE_UPDATE):
+            self.left_arm_pose.pose = self.clip_to_arm_bounds(feedback.pose, 'LEFT')
+        elif (event_type == feedback.MOUSE_DOWN):
+            pass
+        elif (event_type == feedback.MOUSE_UP):
+            pass
+        else:
+            rospy.logerr("Left arm - unrecognized feedback type - " + str(feedback.event_type))
+        self.update()
+
+    def r_arm_feedback_cb(self, feedback):
+        event_type = feedback.event_type
+        if (event_type == feedback.POSE_UPDATE):
+            self.right_arm_pose.pose = self.clip_to_arm_bounds(feedback.pose, 'RIGHT')
+        elif (event_type == feedback.MOUSE_DOWN):
+            pass
+        elif (event_type == feedback.MOUSE_UP):
+            pass
+        else:
+            rospy.logerr("rightt arm - unrecognized feedback type - " + str(feedback.event_type))
         self.update()
 
     def clip_to_arm_bounds(self, arm_target, arm_code):
@@ -153,12 +183,34 @@ class HuboMarkerTeleop:
         return arm_target
 
     def make_gripper_imarker(self, marker_pose, marker_name):
+        control_pose = Pose()
+        control_pose.position.x = marker_pose.pose.position.x
+        control_pose.position.y = marker_pose.pose.position.y
+        control_pose.position.z = marker_pose.pose.position.z + 1.0
+        control_pose.orientation.w = 1.0
+        control_pose_stamped = PoseStamped()
+        control_pose_stamped.pose = control_pose
+        control_pose_stamped.header.frame_id = marker_pose.header.frame_id
+        new_marker = InteractiveMarker()
+        new_marker.header.frame_id = marker_pose.header.frame_id
+        new_marker.pose = control_pose
+        new_marker.scale = 1.0
+        new_marker.name = marker_name
+        new_control = InteractiveMarkerControl()
+        new_control.interaction_mode = InteractiveMarkerControl.MENU
+        new_control.always_visible = True
+        new_control.orientation_mode = InteractiveMarkerControl.VIEW_FACING
+        display_marker = self.make_gripper_marker(control_pose_stamped, marker_name)
+        new_control.markers.append(display_marker)
+        new_marker.controls.append(new_control)
+        return new_marker
+
+    def make_arm_imarker(self, marker_pose, marker_name):
         new_marker = InteractiveMarker()
         new_marker.header.frame_id = marker_pose.header.frame_id
         new_marker.pose = marker_pose.pose
         new_marker.scale = 0.25
         new_marker.name = marker_name
-        
         # Make the default control for the marker itself
         base_control = InteractiveMarkerControl()
         base_control.orientation_mode = InteractiveMarkerControl.FIXED
@@ -166,13 +218,6 @@ class HuboMarkerTeleop:
         display_marker = self.make_arm_marker(marker_pose, marker_name)
         base_control.markers.append(display_marker)
         new_marker.controls.append(base_control)
-        new_control = InteractiveMarkerControl()
-        new_control.interaction_mode = InteractiveMarkerControl.MENU
-        new_control.always_visible = True
-        new_control.orientation_mode = InteractiveMarkerControl.INHERIT
-        display_marker = self.make_arm_marker(marker_pose, marker_name)
-        new_control.markers.append(display_marker)
-        new_marker.controls.append(new_control)
         # Make the x-axis control
         new_control = InteractiveMarkerControl()
         new_control.name = "translate_x"
@@ -240,6 +285,28 @@ class HuboMarkerTeleop:
         new_control.orientation.z = 1.0
         new_marker.controls.append(new_control)
         return new_marker
+
+    def make_gripper_marker(self, marker_pose, marker_name):
+        marker = Marker()
+        marker.header.frame_id = marker_pose.header.frame_id
+        marker.pose = marker_pose.pose
+        #Give it a unique ID
+        marker.ns = marker_name
+        marker.id = 1
+        #Give the marker a type
+        marker.type = Marker.TEXT_VIEW_FACING
+        if ("left" in marker_name.lower()):
+            marker.text = "LEFT"
+        elif ("right" in marker_name.lower()):
+            marker.text = "RIGHT"
+        marker.scale.z = 0.25
+        #Set the color -- be sure to set alpha to something non-zero!
+        marker.color.r = 1.0
+        marker.color.b = 1.0
+        marker.color.g = 0.0
+        marker.color.a = 0.75
+        marker.lifetime = rospy.Duration(0.5)
+        return marker
 
     def make_arm_marker(self, marker_pose, marker_name):
         marker = Marker()
